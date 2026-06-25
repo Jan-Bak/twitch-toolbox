d91e8170f66bcf189f7c1f3c143a121c6ef755c4
BerryJohn <jan.pawel.bak01@gmail.com>
2026-06-26 00:51:01 +0200

    added eslint [forgot about deps]

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import reactPlugin from '@eslint-react/eslint-plugin'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-config-prettier'
+import vitest from '@vitest/eslint-plugin'
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules', 'src-tauri'] },
+
+  js.configs.recommended,
+
+  {
+    files: ['*.config.ts', '*.config.js', 'eslint.config.js'],
+    extends: [...tseslint.configs.recommended],
+  },
+
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    extends: [...tseslint.configs.recommendedTypeChecked],
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    ...reactPlugin.configs['recommended-type-checked'],
+  },
+
+  // react-refresh
+  {
+    files: ['src/**/*.tsx'],
+    plugins: { 'react-refresh': reactRefresh },
+    rules: {
+      'react-refresh/only-export-components': 'warn',
+    },
+  },
+
+  // Vitest
+  {
+    files: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', '**/*.spec.tsx'],
+    plugins: { vitest },
+    rules: {
+      ...vitest.configs.recommended.rules,
+    },
+  },
+
+  prettier,
+)
