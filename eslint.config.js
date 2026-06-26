555cd9d9c91862e8f21636c64ceea963653ef581
BerryJohn <jan.pawel.bak01@gmail.com>
2026-06-26 12:28:21 +0200

    fixed linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import js from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import reactPlugin from '@eslint-react/eslint-plugin'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import prettier from 'eslint-config-prettier'
-import vitest from '@vitest/eslint-plugin'
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactPlugin from '@eslint-react/eslint-plugin';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-config-prettier';
+import vitest from '@vitest/eslint-plugin';
 
 export default tseslint.config(
   { ignores: ['dist', 'node_modules', 'src-tauri'] },
@@ -27,7 +27,23 @@ export default tseslint.config(
   },
 
   {
+    files: ['components/**/*.ts', 'components/**/*.tsx', 'lib/**/*.ts', 'lib/**/*.tsx'],
+    extends: [...tseslint.configs.recommendedTypeChecked],
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+
+  {
     files: ['src/**/*.ts', 'src/**/*.tsx'],
+    ...reactPlugin.configs['recommended-type-checked'],
+  },
+
+  {
+    files: ['components/**/*.tsx', 'src/**/*.tsx'],
     ...reactPlugin.configs['recommended-type-checked'],
   },
 
@@ -49,5 +65,5 @@ export default tseslint.config(
     },
   },
 
-  prettier,
-)
+  prettier
+);
