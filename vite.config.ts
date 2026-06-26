03d2df46b4ae4e334f7fd843349d83f10dc304a4
BerryJohn <jan.pawel.bak01@gmail.com>
2026-06-27 01:36:40 +0200

    tanstack ruter ready

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
